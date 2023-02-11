0e3e1fdc42e4b38a7f7c53d63c74935662bbe81d
naveengitboi <naveenjangiti9@gmail.com>
2023-02-11 21:41:04 +0530

    not integrated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar(){
                 <ul>
                     <NavLink to='/'>Home</NavLink>
                     <NavLink to='/product'>Products</NavLink>
-                    <NavLink to='/login'>Log in</NavLink>
+                    {/* <NavLink to='/login'>Log in</NavLink> */}
                     <NavLink to='/signup'>Sign up</NavLink>
                 </ul>
             </header>
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,14 +1,62 @@
-import React from "react";
+import {React, useState} from "react";
 
 const Signup = ()=> {
+    const [name, setName]= useState('')
+    const [email, setEmail]= useState('')
+    const [password, setPassword]= useState('')
+    const [passErrorMsg, setPassErrorMsg]= useState('')
+    const [nameErrorMsg, setNameErrorMsg]= useState('')
+    const [emailErrorMsg, setEmailErrorMsg]= useState('')
+
+    function collectData(){
+        console.log(name, email, password)
+    }
+
+    const varifyData = () => {
+        if(name == '' && email == '' && password == ''){
+           setNameErrorMsg(prev => prev = '*Required')
+           setEmailErrorMsg(prev => prev = '*Required')
+           setPassErrorMsg(prev => prev = '*Required')
+        }
+        else if(name == '' && email != '' && password != '') {
+            setNameErrorMsg(prev => prev = '*Required')
+            setEmailErrorMsg(prev => prev = '')
+            setPassErrorMsg(prev => prev = '')
+        }
+          else if(name != '' && email == '' && password != '') {
+            setEmailErrorMsg(prev => prev = '*Required')
+            setNameErrorMsg(prev => prev = '')
+            setPassErrorMsg(prev => prev = '')
+        }
+          else if(name != '' && email != '' && password == '') {
+            setPassErrorMsg(prev => prev = '*Required')
+            setEmailErrorMsg(prev => prev = '')
+            setNameErrorMsg(prev => prev = '')
+        }
+        else if(name != '' && email != '' && password != ''){
+            setNameErrorMsg(prev => prev = '')
+            setEmailErrorMsg(prev => prev = '')
+            setPassErrorMsg(prev => prev = '')
+            collectData()
+        }
+    }
+
     return(
         <>
             <div className="page">
                 <div className="form">
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="password" />
-                    <button className="btn" type="submit">Sign Up</button>
+                    <input type="text" value={name} 
+                    onChange= {(e)=> setName((prev) => prev = e.target.value)} placeholder="Name" />
+                    <p className="errorMsg">{nameErrorMsg}</p>
+
+                    <input type="email" value={email} 
+                    onChange= {(e)=> setEmail((prev) => prev = e.target.value)}  placeholder="email" />
+                    <p className="errorMsg">{emailErrorMsg}</p>
+
+                    <input type="password" value={password} 
+                    onChange= {(e)=> setPassword((prev) => prev = e.target.value)}  placeholder="password" />
+                    <p className="errorMsg">{passErrorMsg}</p>
+                    <button className="btn" onClick={varifyData} >Sign Up</button>
                 </div>
             </div>
         </>
